c77a7d1107aad774065e4e7cc46423d80ca83842
ivory <jsivory@itcom666.com>
2024-08-13 21:08:00 +0800

    2024-8-13 21:08

--- src/components/loadingButton/constants.tsx
+++ src/components/loadingButton/constants.tsx
@@ -49,3 +49,27 @@ export const dotDom = () => {
         </svg>
     );
 };
+
+export const broccoli = () => {
+    return (
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'>
+            <path
+                opacity='.25'
+                d='M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4'
+            />
+            <path
+                d='M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z'
+                transform='rotate(84.0713 16 16)'
+            >
+                <animateTransform
+                    attributeName='transform'
+                    type='rotate'
+                    from='0 16 16'
+                    to='360 16 16'
+                    dur='0.8s'
+                    repeatCount='indefinite'
+                />
+            </path>
+        </svg>
+    );
+};
